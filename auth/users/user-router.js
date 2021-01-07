7a9802a910cf8ecb24ac6dc2be67072438999124
Phil Lisak <phil.lisak@gmail.com>
2021-01-06 18:04:30 -0700

    WIP

--- auth/users/user-router.js
+++ auth/users/user-router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const Users = require('./users-model');
 
-router.get('/', (req, res) => {
+router.get('/users', (req, res) => {
 	Users.find()
 		.then((users) => {
 			res.status(200).json(users);
